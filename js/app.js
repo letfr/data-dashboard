4972bb99a7aa2ac99323edadcb7f688c1d96e355
Letícia Faria <leticianfaria@hotmail.com>
2018-07-13 10:33:17 -0300

    First commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
-/*
- * Funcionalidad de tu producto
- */
+// VARIÁVEIS GLOBAIS
+let btnLogin = document.querySelector(".btn-login");
+let intro = document.querySelector(".intro");
+let login = document.querySelector(".login");
 
-// Puedes hacer uso de la base de datos a través de la variable `data`
+function logIn(event) {
+  event.preventDefault();
+  let log = 0;
+  let username = document.querySelector(".username").value;
+  let password = document.querySelector(".password").value;
+
+  if (username == "juamoasei" && password == "gatos") { loginAux(); }
+  else if (username == "rafacerri" && password == "123456789") { loginAux(); }
+  else if (username == "vanessapinheiro" && password == "laboratoria") { loginAux(); }
+  else { alert("Senha ou Usuário inválido."); }
+}
+function loginAux(){
+  intro.classList.remove("none"); 
+  login.classList.add("none");
+}
+
+btnLogin.addEventListener("click",logIn);
+
 console.log(data);
