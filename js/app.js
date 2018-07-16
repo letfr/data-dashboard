5681013b1b1a8ad118218d2e957f9f0b15c3be33
Letícia Faria <leticianfaria@hotmail.com>
2018-07-16 17:09:58 -0300

    Update

--- js/app.js
+++ js/app.js
@@ -79,12 +79,34 @@ function drawChart(activeStudents,dropoutStudents) {
   var options =  {
     title: 'Total de alunas: ' + (activeStudents + dropoutStudents).toString(),
     backgroundColor : "#f2f2f2",
+    chartArea: {width: '100%', height: '90%'},
     is3D: true,
     slices: {
       0: { color: '#F47D91' },
       1: { color: '#62BAA4' }
     }
   };
+  chart.draw(data, options);
+}
+// GRÁFICO NPS
+function drawStacked(nps,promoters,passive,detractors) {
+  var data = google.visualization.arrayToDataTable([
+    ['Name', '%', { role: 'style' }],
+    ['NPS', nps, "#F47D91"],
+    ['Promoters', promoters, "#62BAA4"],
+    ['Passive', passive, "#00A7A9"],
+    ['Detractors', detractors, "#FFCD5A"]
+  ]);
+
+  var options = {
+    chartArea: {width: '60%', height: '90%'},
+    isStacked: true,
+    backgroundColor : "#f2f2f2",
+    legend: {
+    position: "none"
+    }
+  };
+  var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
   chart.draw(data, options);
 }
 // ENVIANDO INFORMAÇÕES PARA DASHBOARD
@@ -137,6 +159,7 @@ function dashboardInfo(sd, trm) {
     ratingTeacher += data[sd][trm]["ratings"][i]["teacher"];
     ratingJedi += data[sd][trm]["ratings"][i]["jedi"];
   }
+  drawStacked(totalNps,promoters,passive,detractors);
   studentsStatus(sd, trm, std, ratings, overcomes, reaches);
   npsPercentual(totalNps, promoters, passive, detractors);
   studentsSatisfaction(overcomes, reaches, sprints);
@@ -195,7 +218,10 @@ console.table(data);
 function googleTranslateElementInit2() { new google.translate.TranslateElement({ pageLanguage: 'pt-br', autoDisplay: false }, 'google_translate_element2'); }
 eval(function (p, a, c, k, e, r) { e = function (c) { return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36)) }; if (!''.replace(/^/, String)) { while (c--) r[e(c)] = k[c] || e(c); k = [function (e) { return r[e] }]; e = function () { return '\\w+' }; c = 1 }; while (c--) if (k[c]) p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]); return p }('6 7(a,b){n{4(2.9){3 c=2.9("o");c.p(b,f,f);a.q(c)}g{3 c=2.r();a.s(\'t\'+b,c)}}u(e){}}6 h(a){4(a.8)a=a.8;4(a==\'\')v;3 b=a.w(\'|\')[1];3 c;3 d=2.x(\'y\');z(3 i=0;i<d.5;i++)4(d[i].A==\'B-C-D\')c=d[i];4(2.j(\'k\')==E||2.j(\'k\').l.5==0||c.5==0||c.l.5==0){F(6(){h(a)},G)}g{c.8=b;7(c,\'m\');7(c,\'m\')}}', 43, 43, '||document|var|if|length|function|GTranslateFireEvent|value|createEvent||||||true|else|doGTranslate||getElementById|google_translate_element2|innerHTML|change|try|HTMLEvents|initEvent|dispatchEvent|createEventObject|fireEvent|on|catch|return|split|getElementsByTagName|select|for|className|goog|te|combo|null|setTimeout|500'.split('|'), 0, {}))
 
-//GRAFICO DE TODAS AS ALUNAS (https://developers.google.com/chart/interactive/docs/reference#dataparam)
+//GRAFICOS
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
+
+google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.setOnLoadCallback(drawStacked);
 
